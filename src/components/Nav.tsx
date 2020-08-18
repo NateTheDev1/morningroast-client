794200190d0d2495474cce3ddcc894fd6cce58cc
NateTheDev1 <nthnlrichards@gmail.com>
2020-08-18 18:49:41 -0400

    Nav Done

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,46 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Nav.css";
 
 const Nav = () => {
   return (
     <nav className="nav">
-      <section className="nav-logo">
-        <i className="fas fa-coffee" />
-        <h1 className="logo-header">Morning Roast Coffee</h1>
-      </section>
-      <section className="nav-left">
-        <Link to="/home" className="nav-link">
-          HOME
-        </Link>
-        <Link to="/menu" className="nav-link">
-          OUR MENU
-        </Link>
-        <a
-          href="https://store.bhappylounge.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="nav-link"
-        >
-          SHOP
-        </a>
-      </section>
-      <section className="nav-right">
-        <section className="nav-locations">
-          <i className="fas fa-map-marker" />
-          <a
-            href="https://www.biggby.com/locations/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="nav-link"
-          >
-            FIND A STORE
-          </a>
+      <section className="nav-inner">
+        <section className="nav-left">
+          <section className="nav-logo">
+            <i className="fas fa-coffee" />
+            <h1 className="logo-header">
+              MORNING <span className="logo-header-middle">ROAST</span> COFFEE
+            </h1>
+          </section>
+          <div className="nav-left-links">
+            <Link to="/home" className="nav-link">
+              HOME
+            </Link>
+            <Link to="/menu" className="nav-link">
+              OUR MENU
+            </Link>
+            <a
+              href="https://store.bhappylounge.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="nav-link"
+            >
+              SHOP
+            </a>
+          </div>
         </section>
-        <button className="nav-btn">SIGN IN</button>
-        <button className="nav-btn nav-btn-cart">
-          <i className="fas fa-shopping-cart"></i>
-          CART
-        </button>
+        <section className="nav-right">
+          <section className="nav-locations">
+            <i className="fas fa-map-marker" />
+            <a
+              href="https://www.biggby.com/locations/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="nav-link"
+            >
+              FIND A STORE
+            </a>
+          </section>
+          <div>
+            <Link to="/cart">CART</Link>
+          </div>
+          <Link to="/login" className="nav-btn">
+            SIGN IN
+          </Link>
+        </section>
       </section>
     </nav>
   );
