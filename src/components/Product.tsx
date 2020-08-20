8743d89e5c14adec7118698fdfb61cfdee1482a2
NateTheDev1 <nthnlrichards@gmail.com>
2020-08-20 01:37:27 -0400

    Added notifs

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Product.css";
 import LazyLoad from "react-lazyload";
 import { useDispatch } from "react-redux";
 import { updateCart } from "../actions/cart";
+import Snackbar from "@material-ui/core/Snackbar";
+import Alert from "@material-ui/lab/Alert";
 
 type ProductType = {
   id: number;
@@ -14,9 +16,12 @@ type ProductType = {
 const Product = ({ productInfo }: { productInfo: ProductType }) => {
   const dispatch = useDispatch();
 
+  const [alertOpen, setAlertOpen] = useState(false);
+
   // Placeholder for adding to cart
   function handleClick() {
     dispatch(updateCart(productInfo));
+    setAlertOpen(true);
   }
 
   return (
@@ -26,6 +31,19 @@ const Product = ({ productInfo }: { productInfo: ProductType }) => {
       </LazyLoad>
       <p className="title">{productInfo.title}</p>
       <p className="price">${productInfo.price}</p>
+      <Snackbar
+        open={alertOpen}
+        autoHideDuration={1500}
+        onClose={() => setAlertOpen(false)}
+      >
+        <Alert
+          icon={false}
+          onClose={() => setAlertOpen(false)}
+          style={{ fontSize: "1.2rem", background: "#F47920", color: "white" }}
+        >
+          Added To Cart!
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
