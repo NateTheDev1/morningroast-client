5ae467ea189b2502595eab2a3f17f4ae5fe59c38
NateTheDev1 <nthnlrichards@gmail.com>
2020-08-20 00:43:46 -0400

    Products grid setup

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,23 +2,34 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../actions/products";
 import "./Menu.css";
+import Product from "./Product";
+
+type ProductType = {
+  id: number;
+  image: string;
+  price: number;
+  title: string;
+};
 
 const Menu = () => {
   const dispatch = useDispatch();
-  const productsData: Array<Object> = useSelector(
+  const productsData: Array<ProductType> = useSelector(
     (state: any) => state.productsReducer.products
   );
-  const loading: boolean = useSelector(
-    (state: any) => state.globalReducer.loading
-  );
-
-  const [products, setProducts]: any = useState(productsData);
+  // const loading: boolean = useSelector(
+  //   (state: any) => state.globalReducer.loading
+  // );
 
   useEffect(() => {
     dispatch(fetchProducts());
   }, [dispatch]);
 
-  function mapProducts() {}
+  function mapProducts() {
+    const els = productsData.map((p: ProductType) => {
+      return <Product productInfo={p} />;
+    });
+    return els;
+  }
 
   return (
     <div className="menu-root">
@@ -31,7 +42,8 @@ const Menu = () => {
           espresso, and tea options!
         </p>
       </div>
-      <hr className="divider" />
+
+      <div className="products-grid">{mapProducts()}</div>
     </div>
   );
 };
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -9,7 +9,13 @@ type ProductType = {
 };
 
 const Product = ({ productInfo }: { productInfo: ProductType }) => {
-  return <div className="product-card"></div>;
+  return (
+    <div className="product-card" key={productInfo.id}>
+      <img src={productInfo.image} alt={productInfo.title} />
+      <h2>{productInfo.title}</h2>
+      <p>${productInfo.price}</p>
+    </div>
+  );
 };
 
 export default Product;
